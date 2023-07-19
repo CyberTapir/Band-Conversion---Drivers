21361120cbe11760649240b722e4a8e2f6a771b5
Callum Bennett <callumtbennett@gmail.com>
2023-07-19 14:59:34 +1000

    x

--- Form1.vb
+++ Form1.vb
@@ -4,36 +4,35 @@
     End Sub
     Private Sub driver()
         ListBox1.Items.Add("Ted")
-        ListBox1.Items.Add(PerfBandTed(80, 2))
-        ListBox1.Items.Add(PerfBandTed(90, 2))
-        ListBox1.Items.Add(PerfBandTed(80, 1))
-        ListBox1.Items.Add(PerfBandTed(80, 100))
-        ListBox1.Items.Add(PerfBandTed(110, 100))
-        ListBox1.Items.Add(PerfBandTed(0, 0))
+        ListBox1.Items.Add("Input 80 2 " & PerfBandTed(80, 2))
+        ListBox1.Items.Add("Input 90 2 " & PerfBandTed(90, 2))
+        ListBox1.Items.Add("Input 80 1 " & PerfBandTed(80, 1))
+        ListBox1.Items.Add("Input 80 100 " & PerfBandTed(80, 100))
+        ListBox1.Items.Add("Input 110 100 " & PerfBandTed(110, 100))
+        'ListBox1.Items.Add("Input 0 0 " & PerfBandTed(0, 0))
         'ListBox1.Items.Add(PerfBandTed("Mr", "Gray"))
-        ListBox1.Items.Add(PerfBandTed(-80, -100))
+        ListBox1.Items.Add("Input -80 -100 " & PerfBandTed(-80, -100))
         ListBox1.Items.Add("Mary")
-        ListBox1.Items.Add(PerfBandMary(80, 2))
-        ListBox1.Items.Add(PerfBandMary(90, 2))
-        ListBox1.Items.Add(PerfBandMary(80, 1))
-        ListBox1.Items.Add(PerfBandMary(80, 100))
-        ListBox1.Items.Add(PerfBandMary(110, 100))
-        ListBox1.Items.Add(PerfBandMary(0, 0))
-        ListBox1.Items.Add(PerfBandMary("Mr", "Gray"))
-        ListBox1.Items.Add(PerfBandMary(-80, -100))
+        ListBox1.Items.Add("Input 80 2 " & PerfBandMary(80, 2))
+        ListBox1.Items.Add("Input 90 2 " & PerfBandMary(90, 2))
+        ListBox1.Items.Add("Input 80 1 " & PerfBandMary(80, 1))
+        ListBox1.Items.Add("Input 80 100 " & PerfBandMary(80, 100))
+        ListBox1.Items.Add("Input 110 100 " & PerfBandMary(110, 100))
+        ListBox1.Items.Add("Input 0 0 " & PerfBandMary(0, 0))
+        ListBox1.Items.Add("Input Mr Gray " & PerfBandMary("Mr", "Gray"))
+        ListBox1.Items.Add("Input -80 -100 " & PerfBandMary(-80, -100))
         ListBox1.Items.Add("Alice")
-        ListBox1.Items.Add(PerfBandAlice(80, 2))
-        ListBox1.Items.Add(PerfBandAlice(90, 2))
-        ListBox1.Items.Add(PerfBandAlice(80, 1))
-        ListBox1.Items.Add(PerfBandAlice(80, 100))
-        ListBox1.Items.Add(PerfBandAlice(110, 100))
-        ListBox1.Items.Add(PerfBandAlice(0, 0))
+        ListBox1.Items.Add("Input 80 2 " & PerfBandAlice(80, 2))
+        ListBox1.Items.Add("Input 90 2 " & PerfBandAlice(90, 2))
+        ListBox1.Items.Add("Input 80 1 " & PerfBandAlice(80, 1))
+        ListBox1.Items.Add("Input 80 100 " & PerfBandAlice(80, 100))
+        ListBox1.Items.Add("Input 110 100 " & PerfBandAlice(110, 100))
+        'ListBox1.Items.Add("Input 0 0 " & PerfBandAlice(0, 0))
         'ListBox1.Items.Add(PerfBandAlice("Mr", "Gray"))
-        ListBox1.Items.Add(PerfBandAlice(-80, -100))
+        ListBox1.Items.Add("Input -80 -100 " & PerfBandAlice(-80, -100))
     End Sub
 
     Public Function PerfBandTed(Mark As Integer, Unit As Integer) As Integer
-        On Error GoTo InputError
         Dim PMark As Integer, Band As Integer
         PMark = Mark * 2 / Unit
         If PMark < 50 Then
@@ -50,8 +49,6 @@
             Band = 6
         End If
         PerfBandTed = Band
-InputError:
-        PerfBandTed = (0)
     End Function
 
     Public Function PerfBandMary(Mark As Object, Unit As Object) As Integer
@@ -69,7 +66,6 @@ InputError:
     End Function
 
     Public Function PerfBandAlice(Mark As Integer, UnitValue As Integer) As Integer
-        On Error GoTo InputError
         Dim Percent As Integer, Band As Integer
         'Convert to a mark out of 100
         Percent = Mark / (UnitValue * 50) * 100
@@ -85,7 +81,5 @@ InputError:
         End Select
         'Return performance band
         PerfBandAlice = Band
-InputError:
-        PerfBandAlice = (-1)
     End Function
 End Class
